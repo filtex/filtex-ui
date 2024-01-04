ea9ca6811f4ec00b62a1d816ff53b9080e9fc566
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-04 23:24:35 +0300

    feat: create editor component with syntax highlighting

---
+++ src/components/Editor/Editor.tsx
@@ -0,0 +1,112 @@
+import * as React from "react";
+
+import './Editor.css';
+
+export interface OnChangeOptions {
+    value: string;
+    cursor: OnChangeCursorOptions;
+}
+
+export interface OnChangeCursorOptions {
+    start: number;
+    end: number;
+}
+
+export interface EditorProps {
+    label?: string;
+    value: string;
+    onChange: (opts: OnChangeOptions) => void;
+    coloringFn: (value: string) => string;
+    inputRef: React.RefObject<HTMLTextAreaElement>;
+}
+
+const Editor = (props: EditorProps) => {
+    const preRef = React.useRef<HTMLPreElement>() as React.MutableRefObject<HTMLPreElement>;
+    const {
+        label,
+        value,
+        onChange,
+        coloringFn
+    } = props;
+
+    const handleChange = (e: any) => {
+        if (onChange) {
+            onChange({
+                value: e.target.value,
+                cursor: {
+                    start: e.target.selectionStart,
+                    end: e.target.selectionEnd
+                }
+            });
+        }
+    };
+
+    const handleFocus = (e: any) => {
+        if (onChange) {
+            onChange({
+                value: e.target.value,
+                cursor: {
+                    start: e.target.selectionStart,
+                    end: e.target.selectionEnd
+                }
+            });
+        }
+    };
+
+    const handleBlur = (e: any) => {
+        if (onChange) {
+            onChange({
+                value: e.target.value,
+                cursor: {
+                    start: -1,
+                    end: -1
+                }
+            });
+        }
+    };
+
+    const handleClick = (e: any) => {
+        setTimeout(() => {
+            if (onChange) {
+                onChange({
+                    value: e.target.value,
+                    cursor: {
+                        start: e.target.selectionStart,
+                        end: e.target.selectionEnd
+                    }
+                });
+            }
+        }, 100);
+    };
+
+    const handleScrollCapture = (e: any) => {
+        preRef.current.scrollLeft = e.target.scrollLeft;
+    };
+
+    const html = coloringFn(value);
+
+    return (
+        <div className="editor">
+            <textarea
+                ref={props.inputRef}
+                value={value}
+                onChange={handleChange}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                onClick={handleClick}
+                onScrollCapture={handleScrollCapture}
+                placeholder={label}
+                autoCapitalize="off"
+                autoComplete="off"
+                autoCorrect="off"
+                spellCheck={false}
+                rows={1} />
+            <pre
+                ref={preRef}
+                hidden={true}
+                dangerouslySetInnerHTML={{ __html: html + '<br />' }} />
+        </div>
+    );
+};
+
+export default Editor;
