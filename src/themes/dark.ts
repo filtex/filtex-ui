dba9f7cff99728c4a27c7fd1f1ce56e7cb678105
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-04 23:04:06 +0300

    feat: add dark and light themes

---
+++ src/themes/dark.ts
@@ -0,0 +1,22 @@
+import { FiltexTheme } from "./core";
+
+export const DarkTheme: FiltexTheme = {
+    name: 'dark',
+    style: {
+        backgroundColor: '#282A2E',
+        hoverBackgroundColor: '#3a3d43',
+        activeBackgroundColor: '#222',
+        textColor: '#ccc',
+        selectionColor: '#696969',
+        logicTextColor: '#e5c07b',
+        fieldTextColor: '#61afef',
+        operatorTextColor: '#c678dd',
+        valueTextColor: '#e06c75',
+        separatorTextColor: '#56b6c2',
+        bracketTextColor: '#ffd700',
+        invalidBorderColor: '#ae0000',
+        boxShadow: '0 0 5px #111',
+        border: 'none',
+        borderRadius: '5px'
+    }
+};
---
+++ src/themes/light.ts
@@ -0,0 +1,22 @@
+import { FiltexTheme } from "./core";
+
+export const LightTheme: FiltexTheme = {
+    name: 'light',
+    style: {
+        backgroundColor: '#e1e1e1',
+        hoverBackgroundColor: '#d5d5d5',
+        activeBackgroundColor: '#c5c5c5',
+        textColor: '#555',
+        selectionColor: '#d5d5d5',
+        logicTextColor: '#1986af',
+        fieldTextColor: '#407de9',
+        operatorTextColor: '#993896',
+        valueTextColor: '#d45e53',
+        separatorTextColor: '#56b6c2',
+        bracketTextColor: '#5670f0',
+        invalidBorderColor: '#ae0000',
+        boxShadow: '0 0 5px gray',
+        border: 'none',
+        borderRadius: '5px'
+    }
+};
