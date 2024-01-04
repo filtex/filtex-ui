a8a0c5aab93e8b0c7f94c7b97fad54f1f4dd97ef
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-04 23:44:31 +0300

    feat: handle arrow, enter, tab, esc keys for dropdown component

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -51,6 +51,72 @@ const Dropdown = (props: DropdownProps) => {
         }
     }, [onValueChange]);
 
+    const handleKeyDown = useCallback((e: any) => {
+        if (!props.options) {
+            return;
+        }
+
+        const { type } = props.options;
+
+        if (values.length === 0 && type === 'options') {
+            return;
+        }
+
+        const { key, keyCode, ctrlKey } = e;
+        switch (key) {
+            case "Escape":
+                e.preventDefault();
+                setHidden(true);
+                break;
+            case "Enter":
+            case "Tab":
+                if (!hidden && !ctrlKey) {
+                    e.preventDefault();
+                    if (option !== -1) {
+                        handleValueChange(values[option].value);
+                    }
+                    setOption(0);
+                    if (type !== 'options' || values.length > 0) {
+                        setHidden(false);
+                    }
+                }
+                break;
+            case "ArrowUp":
+                e.preventDefault();
+                if (option === -1 || option === 0) {
+                    setOption(values.length - 1);
+                    scrollTo(values.length - 1);
+                } else {
+                    setOption(option - 1);
+                    scrollTo(option - 1);
+                }
+                break;
+            case "ArrowDown":
+                e.preventDefault();
+                if (option !== values.length - 1) {
+                    setOption(option + 1);
+                    scrollTo(option + 1);
+                } else {
+                    setOption(0);
+                    scrollTo(0);
+                }
+                break;
+            default:
+                const isAlphanumeric =
+                    (keyCode >= 48 && keyCode <= 57) ||
+                    (keyCode >= 65 && keyCode <= 90) ||
+                    (keyCode >= 97 && keyCode <= 122);
+                if (elementRef && document.activeElement === elementRef && isAlphanumeric) {
+                    if (type !== 'options' || values.length > 0) {
+                        setOption(0);
+                        scrollTo(0);
+                        setHidden(false);
+                    }
+                }
+                break;
+        }
+    }, [elementRef, handleValueChange, hidden, option, props.options, scrollTo, values]);
+
     const handleClick = useCallback((ev: any) => {
         if (!props.options) {
             return;
@@ -113,11 +179,13 @@ const Dropdown = (props: DropdownProps) => {
         }
 
         window.addEventListener("click", handleClick);
+        window.addEventListener("keydown", handleKeyDown);
 
         return () => {
             window.removeEventListener("click", handleClick);
+            window.removeEventListener("keydown", handleKeyDown);
         };
-    }, [elementRef, onValueChange, hidden, coordinates, values, option, handleClick]);
+    }, [elementRef, onValueChange, hidden, coordinates, values, option, handleClick, handleKeyDown]);
 
     useEffect(() => {
         if (props.options) {
