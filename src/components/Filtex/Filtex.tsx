1486439ef92c211da8a17923f864969c62927ad3
Oguzhan Kiyar <oguzhan.kiyar@outlook.com>
2024-01-05 00:01:24 +0300

    feat: implement query text and tree components in filtex component

--- src/components/Filtex/Filtex.tsx
+++ src/components/Filtex/Filtex.tsx
@@ -1,20 +1,109 @@
-import React, {createRef, useEffect, useState} from "react";
+import React, {createRef, useCallback, useEffect, useMemo, useState} from "react";
+import {Metadata} from "../../models";
+import {JsonQueryConverter, TextQueryConverter} from "../../converters";
+import QueryText from "../QueryText/QueryText";
+import QueryTree from "../QueryTree/QueryTree";
 import Dropdown from "../Dropdown/Dropdown";
 import {DropdownContext, ThemeContext} from "../../contexts";
 import {DefaultTheme, FiltexTheme, Themes} from "../../themes";
-import {toRGB} from "../../utils";
+import {isArray, toRGB} from "../../utils";
 
 import './Filtex.css';
 
 export interface FiltexProps {
+    metadata: Metadata;
     themes?: FiltexTheme[];
     theme?: string;
+    modes?: string[];
+    mode?: string;
+    value?: any;
+    onSubmit?: (value: any) => void;
 }
 
 const Filtex = (props: FiltexProps) => {
+    const [text, setText] = useState<string>('');
+    const [tree, setTree] = useState<any>(null);
     const [theme, setTheme] = useState<FiltexTheme>(DefaultTheme);
+    const [modes] = useState(props.modes || ['text', 'tree']);
+    const [mode, setMode] = useState(props.mode || modes[0]);
+    const [value, setValue] = useState(props.value || '');
     const filtexRef = createRef<HTMLDivElement>();
     const [options, setOptions] = useState<any>({ hidden: true });
+
+    const textQueryConverter = useMemo(() => new TextQueryConverter(props.metadata), [props.metadata]);
+    const jsonQueryConverter = useMemo(() => new JsonQueryConverter(props.metadata), [props.metadata]);
+
+    const handleTextChange = (text: string) => {
+        const tree = textQueryConverter.convert(text);
+
+        setText(text);
+        setTree(tree);
+    };
+
+    const handleTextSubmit = () => {
+        if (!props.onSubmit) {
+            return;
+        }
+
+        if (tree === null || text === '') {
+            props.onSubmit({
+                tree: null,
+                text: ''
+            });
+        } else {
+            props.onSubmit({
+                tree,
+                text
+            });
+        }
+    };
+
+    const handleTreeChange = (tree: any) => {
+        const text = jsonQueryConverter.convert(tree);
+
+        setText(text);
+        setTree(tree);
+    };
+
+    const handleTreeSubmit = () => {
+        if (!props.onSubmit) {
+            return;
+        }
+
+        if (tree === null || text === '') {
+            props.onSubmit({
+                tree: null,
+                text: ''
+            });
+        } else {
+            props.onSubmit({
+                tree,
+                text
+            });
+        }
+    };
+
+    const handleSwitchText = () => {
+        setMode('text');
+    };
+
+    const handleSwitchTree = () => {
+        setMode('tree');
+    };
+
+    const handleSubmit = () => {
+        if (props.onSubmit) {
+            props.onSubmit({
+                tree,
+                text
+            });
+        }
+    };
+
+    const handleReset = () => {
+        setTree(null);
+        setText('');
+    };
 
     useEffect(() => {
         const themes = [...Themes, ...(props.themes || [])];
@@ -44,11 +133,56 @@ const Filtex = (props: FiltexProps) => {
         root.style.setProperty('--border-radius', theme.style.borderRadius);
     }, [props.themes, props.theme, filtexRef])
 
+    useEffect(() => {
+        setValue(props.value || '');
+    }, [props.value]);
+
+    useEffect(() => {
+        if (!value) {
+            handleReset();
+        }
+
+        if (typeof value === 'string') {
+            const tree = textQueryConverter.convert(value ?? '');
+
+            setText(value);
+            setTree(tree);
+        } else if (isArray(value)) {
+            const text = jsonQueryConverter.convert(value ?? []);
+
+            setText(text);
+            setTree(value);
+        } else {
+            handleReset();
+        }
+    }, [jsonQueryConverter, textQueryConverter, value]);
+
     return (
         <ThemeContext.Provider value={theme}>
             <DropdownContext.Provider value={{options, setOptions}}>
                 <div className="filtex" ref={filtexRef}>
-                    Filtex
+                    {
+                        modes.includes('text')
+                            ? <QueryText
+                                className="text-container"
+                                metadata={props.metadata}
+                                value={text}
+                                onValueChange={handleTextChange}
+                                onValueSubmit={handleTextSubmit}
+                                hidden={mode !== 'text'} />
+                            : <></>
+                    }
+                    {
+                        modes.includes('tree')
+                            ? <QueryTree
+                                className="tree-container"
+                                metadata={props.metadata}
+                                value={tree}
+                                onValueChange={handleTreeChange}
+                                onValueSubmit={handleTreeSubmit}
+                                hidden={mode !== 'tree'} />
+                            : <></>
+                    }
                     <Dropdown options={options} />
                 </div>
             </DropdownContext.Provider>
